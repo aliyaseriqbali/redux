6524a03d0616740fdb66b96658a6654493c78efa
ali iqbali <ali.iqbali@frontedgeit.se>
2018-08-29 14:07:05 +0200

    Redux project, displaying books

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function () {
+    return [
+        { title: 'Javascripts: The Good Parts', pages: 102 },
+        { title: 'Harry Potter', pages: 112 },
+        { title: 'The Dark Tower', pages: 50 },
+        { title: 'Eloquent Ruby', pages: 10 }
+    ]
+}
